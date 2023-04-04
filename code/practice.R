32f0a871034d13708c622321c3e98f43c63b9e04
Juliana Bushnell <juliana.bushnell@gmail.com>
2023-04-04 10:30:44 -0400

    practice coding day

---
+++ code/practice.R
@@ -0,0 +1,6 @@
+pacman::p_load(tidyverse, here, dplyr, gtsummary, gt, webshot2,sandwich,stats,here, ggplot2,stats, graphics, splines,boot,ggExtra,formatR,
+               gridExtra,skimr,Hmisc,RColorBrewer, lmtest)
+here::i_am("code/practice.R")
+asthma<-read.csv(here::here("data/asthma.csv"))
+
+
